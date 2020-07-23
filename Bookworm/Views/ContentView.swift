0d537477a2c2d6c55d86857f2d4403cf0277a857
Nate Lee <nateleee.dev@gmail.com>
2020-07-23 16:10:54 +0800

    - [x] Using size classes with AnyView type erasure.

--- Bookworm/Views/ContentView.swift
+++ Bookworm/Views/ContentView.swift
@@ -9,40 +9,16 @@
 import SwiftUI
 
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors: [Color] = [.red, .yellow]
-    var offColors: [Color] = [.init(white: 0.6), .init(white: 0.4)]
-    
-    var body: some View {
-        Button(action: {
-            self.isOn.toggle()
-        }) {
-            Text(title)
-        }
-        .padding()
-        .background(
-            LinearGradient(
-                gradient: .init(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom)
-        )
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-            .shadow(radius: isOn ? 9 : 0)
-        
-    }
-}
-
-
 struct ContentView: View {
-    @State private var rememberMe: Bool = false
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
-        VStack {
-            PushButton(title: "Title Title", isOn: $rememberMe)
-            
-            Text("\(rememberMe ? "On" : "Off")")
+        if (horizontalSizeClass == .compact) {
+            return AnyView(Text("horizontalSizeClass == .compact"))
+        } else {
+            return AnyView(VStack {
+                Text("horizontalSizeClass == .regular")
+            })
         }
     }
 }
