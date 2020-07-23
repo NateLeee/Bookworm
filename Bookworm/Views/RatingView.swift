983c6c6a99a2df12ba0c8274f6224f1615812bc8
Nate Lee <nateleee.dev@gmail.com>
2020-07-23 17:13:32 +0800

    Added a new view file: RatingView.

---
+++ Bookworm/Views/RatingView.swift
@@ -0,0 +1,23 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Nate Lee on 7/23/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rate: Int16
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}
