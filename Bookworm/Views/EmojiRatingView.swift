b167ecf389aad286d2effa84f0ed65cbf3bcd8ec
Nate Lee <nateleee.dev@gmail.com>
2020-07-23 17:27:49 +0800

    Added a new view file: EmojiRatingView.

---
+++ Bookworm/Views/EmojiRatingView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Nate Lee on 7/23/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView()
+    }
+}
